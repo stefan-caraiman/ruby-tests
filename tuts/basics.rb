ece8ca47d62e7bcae2ee75277e8a6c088a62d755
Stefan Caraiman <scaraiman@cloudbasesolutions.com>
2017-01-04 00:21:12 +0200

    Adds other basic Ruby examples

--- tuts/basics.rb
+++ tuts/basics.rb
@@ -11,3 +11,18 @@ puts "You will be " + (age + random_num).to_s + " in " + random_num.to_s + " yea
 num_one = 1.000
 num99 = 0.999
 puts "#{num_one} - #{num99} = #{num_one-num99}"
+
+# Everything is an Object
+puts 1.class
+puts "ana".class
+
+# Defining constants
+SOME_CONTSTANT = 3.14
+SOME_CONTSTANT = 3.145
+puts SOME_CONTSTANT # Will give us a warning
+
+# Sample working with files
+write_handler = File.new("test.txt", "w")
+write_handler.puts("Random").to_s
+write_handler.close
+data_from_file = File.read("test.txt")
